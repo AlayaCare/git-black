788b42c7e3f26f36297c73ee7587d2cbe3b3cd7d
Andy Teijelo <ateijelo@gmail.com>
2020-05-30 12:28:15 -0400

    list changed hunks

--- src/git_black.py
+++ src/git_black.py
@@ -5,8 +5,19 @@ import click
 from git import Repo
 from git.diff import Diff
 
+from unidiff import PatchSet
+
 commit_re = re.compile(rb"(?P<commit>[0-9a-f]{40})\s+\d+\s+(?P<lineno>\d+)")
 
+
+def list_patches():
+    patch_set = PatchSet(Popen(["git", "diff", "--patience", "-U0"], stdout=PIPE).stdout, encoding='latin-1')
+    for mf in patch_set.modified_files:
+        for hunk in mf:
+            print(hunk.source_start, hunk.source_length)
+            print(hunk.target_start, hunk.target_length)
+            for line in hunk:
+                print(repr(line.value))
 
 def git_blame(filename):
     p = Popen(["git", "blame", "-p", filename], stdout=PIPE)
@@ -25,10 +36,12 @@ def git_blame(filename):
 @click.command()
 @click.argument("filename", required=False, default="")
 def cli(filename):
-    repo = Repo(search_parent_directories=True)
-    print(repo)
-    for diff in repo.index.diff(None):
-        print(diff)
+    #repo = Repo(search_parent_directories=True)
+    #print(repo)
+    #for diff in repo.index.diff(None):
+    #    print(diff)
+    list_patches()
+
     # blame = git_blame(filename)
     # for commit, lines in blame.items():
     #    print(commit)
