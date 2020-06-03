8bfc2b237e26fea7e215911fda846e0a5a8e83b4
Andy Teijelo <ateijelo@gmail.com>
2020-06-03 00:59:11 -0400

    try to use the original commit's author & date

--- src/git_black.py
+++ src/git_black.py
@@ -3,12 +3,13 @@ import re
 import shutil
 import sys
 import time
+from bisect import bisect
 from datetime import datetime
 from subprocess import PIPE, Popen, run
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 
 import click
-from git import Repo
+from git import Commit, Repo
 from git.diff import Diff
 from git.util import Actor
 from unidiff import Hunk, PatchSet
@@ -24,12 +25,24 @@ class GitBlack:
     def __init__(self):
         self.repo = Repo(search_parent_directories=True)
 
-    def commit_filename(self, filename):
-        blame_map = sorted(
-            self.repo.blame_incremental("HEAD", filename),
-            key=lambda blame_entry: blame_entry.linenos.start,
-        )
+    def commit_for_line(self, filename, lineno) -> Commit:
+        if not filename in self._blame_starts:
+            blame = sorted(
+                [
+                    (e.linenos.start, e.commit)
+                    for e in self.repo.blame_incremental("HEAD", filename)
+                ]
+            )
+            self._blame_starts[filename] = []
+            self._blame_commits[filename] = []
+            for line, commit in blame:
+                self._blame_starts[filename].append(line)
+                self._blame_commits[filename].append(commit)
 
+        idx = bisect(self._blame_starts[filename], lineno) - 1
+        return self._blame_commits[filename][idx]
+
+    def commit_filename(self, filename):
         with TemporaryDirectory(dir=".") as tmpdir:
             a = os.path.join(tmpdir, "a")
             b = os.path.join(tmpdir, "b")
@@ -63,13 +76,13 @@ class GitBlack:
                 self.apply(a, b, hunk.source_start, hunk.source_length, target_lines)
                 os.rename(b, a)
 
+                original_commit = self.commit_for_line(filename, hunk.source_start)
+
                 self.repo.index.add(a, path_rewriter=lambda entry: filename, write=True)
                 self.repo.index.commit(
                     "hunk {}-{}".format(hunk.source_start, hunk.source_length),
-                    author=Actor("John Doe", "johndoe@example.com"),
-                    author_date=datetime.now()
-                    .replace(minute=0)
-                    .isoformat(timespec="seconds"),
+                    author=original_commit.author,
+                    author_date=original_commit.authored_datetime,
                 )
 
     def apply(
