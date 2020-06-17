8cc24a41d2bd2599c3e7f43f91db40caa789d0dd
Andy Teijelo <ateijelo@gmail.com>
2020-06-17 00:59:33 -0400

    commit all existing changes

--- src/git_black/__init__.py
+++ src/git_black/__init__.py
@@ -1,5 +1,6 @@
 import os
 from bisect import bisect
+from collections import namedtuple
 from dataclasses import dataclass
 from importlib.resources import read_text
 from subprocess import PIPE, Popen, run
@@ -10,7 +11,7 @@ import click
 from git import Commit, Repo
 from git.objects.util import altz_to_utctz_str
 from jinja2 import Environment, FunctionLoader
-from unidiff import Hunk, PatchSet
+from unidiff import Hunk, PatchedFile, PatchSet
 
 
 def load_template(template):
@@ -25,10 +26,11 @@ def reformat(a):
     run(["black", "-l89", a])
 
 
-@dataclass
+@dataclass(frozen=True)
 class Delta:
     """this is a simplified version of unidiff.Hunk"""
 
+    filename: str
     src_start: int
     src_lines: List[str]
     dst_start: int
@@ -47,8 +49,9 @@ class Delta:
         return len(self.dst_lines)
 
     @staticmethod
-    def from_hunk(hunk: Hunk, encoding: str):
+    def from_hunk(hunk: Hunk, filename, encoding: str):
         return Delta(
+            filename=filename,
             src_start=hunk.source_start,
             src_lines=[line.value.encode(encoding) for line in hunk.source_lines()],
             dst_start=hunk.target_start,
@@ -56,14 +59,174 @@ class Delta:
         )
 
     def __str__(self):
-        s = f"Delta(\n    src_start={self.src_start},\n    src_lines=[\n"
+        s = [
+            "Delta(" f"    filename={self.filename},",
+            f"    src_start={self.src_start},",
+            "    src_lines=[",
+        ]
         for line in self.src_lines:
-            s += "        {!r},\n".format(line)
-        s += f"    ],\n    dst_start={self.dst_start},\n    dst_lines=[\n"
+            s.append("        {!r},".format(line))
+        s.extend(["    ],", f"    dst_start={self.dst_start},", "    dst_lines=["])
         for line in self.dst_lines:
-            s += "        {!r},\n".format(line)
-        s += "    ]\n)"
-        return s
+            s.append("        {!r},".format(line))
+        s.append("    ])")
+        return "\n".join(s)
+
+
+DeltaBlame = namedtuple("DeltaBlame", "delta commits")
+
+
+class HunkBlamer:
+    PATCH_ENCODING = "latin-1"
+
+    def __init__(self, repo, filename):
+        self.repo = repo
+        self.filename = filename
+        patch_set = PatchSet(
+            Popen(["git", "diff", "-U0", filename], stdout=PIPE,).stdout,
+            encoding=self.PATCH_ENCODING,
+        )
+        self.modified_file = None
+        if not patch_set.modified_files:
+            return
+        self.modified_file = patch_set.modified_files[0]
+        self._load_blame()
+
+    def _load_blame(self):
+        _blame = sorted(
+            [
+                (e.linenos.start, e.commit)
+                for e in self.repo.blame_incremental("HEAD", self.modified_file.path)
+            ]
+        )
+        self._blame_starts = []
+        self._blame_commits = []
+        for line, commit in _blame:
+            self._blame_starts.append(line)
+            self._blame_commits.append(commit)
+
+    def _blame(self, lineno) -> Commit:
+        idx = bisect(self._blame_starts, lineno) - 1
+        return self._blame_commits[idx]
+
+    def _map_lines(self, delta: Delta):
+        """
+        return a dict that maps tuples of source lines
+        to tuples of destination lines. Each key/value pair
+        in the dict represents a set of source lines (the key tuple)
+        that became the destination lines (the value tuple).
+
+        although weird, the numbers in the tuples are 0-indexed.
+
+        e.g.
+        {   # src: dst
+            (0,1): (0)     # the first 2 lines collapsed into the first line of the output
+            (2,): (1,2,3)  # the third line expanded into lines 2 3 and 4
+        }
+        """
+
+        # this is harder than I thought; I'll start with a super naive
+        # approach and improve it later (or never)
+
+        if delta.src_length == 0:
+            return {(): tuple(range(delta.dst_length))}
+        if delta.dst_length == 0:
+            return {tuple(range(delta.src_length)): ()}
+
+        result = {}
+
+        for i in range(min(delta.src_length, delta.dst_length) - 1):
+            result[(i,)] = (i,)
+
+        if delta.src_length >= delta.dst_length:
+            result[tuple(range(delta.dst_length - 1, delta.src_length))] = (
+                delta.dst_length - 1,
+            )
+        else:
+            result[(delta.src_length - 1,)] = tuple(
+                range(delta.src_length - 1, delta.dst_length)
+            )
+
+        return result
+
+    def blames(self) -> List[DeltaBlame]:
+        if not self.modified_file:
+            return []
+
+        hunk_deltas = [
+            Delta.from_hunk(hunk, self.filename, self.PATCH_ENCODING)
+            for hunk in self.modified_file
+        ]
+
+        # let's map each hunk to its source commits and break down the deltas
+        # in smaller chunks; this will make it possible to prepare and group
+        # commits with a much smaller granularity
+        deltas = []
+        for hd in hunk_deltas:
+            for src_linenos, dst_linenos in self._map_lines(hd).items():
+                ss = hd.src_start + min(src_linenos, default=0)
+                sl = [hd.src_lines[lineno] for lineno in src_linenos]
+                ds = hd.dst_start + min(dst_linenos, default=0)
+                dl = [hd.dst_lines[lineno] for lineno in dst_linenos]
+                delta = Delta(
+                    filename=self.filename,
+                    src_start=ss,
+                    src_lines=sl,
+                    dst_start=ds,
+                    dst_lines=dl,
+                )
+                deltas.append(delta)
+
+        blames = []
+        for i, delta in enumerate(deltas):
+            delta_blame = DeltaBlame(delta=delta, commits=set())
+            for line in range(
+                delta.src_start, delta.src_start + max(1, delta.src_length)
+            ):
+                commit = self._blame(line)
+                delta_blame.commits.add(commit.hexsha)
+            blames.append(delta_blame)
+
+        return blames
+
+
+class Patcher:
+    def __init__(self, repo, filename):
+        self.repo = repo
+        self.filename = filename
+        self._offsets = {}
+        self._lines = Popen(
+            ["git", "show", "HEAD:" + self.filename], stdout=PIPE
+        ).stdout.readlines()
+        self._applied = set()
+
+    def apply(self, delta):
+        if (delta.src_start) in self._applied:
+            return
+
+        src_length = len(delta.src_lines)
+        src_start = delta.src_start
+        for pos, off in self._offsets.items():
+            if delta.src_start > pos:
+                src_start += off
+
+        # I don't understand why, but unified diff needs
+        # this when the source length is 0
+        if src_length == 0:
+            src_start += 1
+
+        i = src_start - 1
+        j = i + src_length
+        self._lines[i:j] = delta.dst_lines
+
+        self._offsets[delta.src_start] = delta.offset
+
+        self._applied.add(delta.src_start)
+
+    def write(self, filename):
+        f = open(filename, "wb")
+        f.writelines(self._lines)
+        f.close()
 
 
 class WorkingFile:
@@ -113,64 +276,61 @@ class GitBlack:
         self.a_html = []
         self.b_html = []
         self.color_idx = 0
+        self.patchers = {}
 
-    def blame(self, filename, lineno) -> Commit:
-        if filename not in self._blame_starts:
-            _blame = sorted(
-                [
-                    (e.linenos.start, e.commit)
-                    for e in self.repo.blame_incremental("HEAD", filename)
-                ]
+    def commit_changes(self):
+        grouped_deltas = {}
+        for diff in self.repo.index.diff(None):
+            if diff.change_type != "M":
+                continue
+            filename = diff.a_path
+            self.patchers[filename] = Patcher(self.repo, filename)
+            hb = HunkBlamer(self.repo, filename)
+            for delta_blame in hb.blames():
+                commits = tuple(sorted(delta_blame.commits))
+                grouped_deltas.setdefault(commits, []).append(delta_blame.delta)
+
+        for commits, deltas in grouped_deltas.items():
+            self._commit(commits, deltas)
+
+    def _commit(self, original_commits, deltas: List[Delta]):
+        print("comitting {}...".format(original_commits))
+        with TemporaryDirectory(dir=".") as tmpdir:
+
+            dirs = set(os.path.dirname(d.filename) for d in deltas)
+            for d in dirs:
+                os.makedirs(os.path.join(tmpdir, d), exist_ok=True)
+
+            for delta in deltas:
+                filename = delta.filename
+                patcher = self.patchers[filename]
+                tmpf = os.path.join(tmpdir, filename)
+
+                patcher.apply(delta)
+                patcher.write(tmpf)
+
+                self.repo.index.add(tmpf, path_rewriter=lambda entry: filename)
+
+            commits = [self.repo.commit(h) for h in original_commits]
+
+            main_commit = commits[0]
+            commit_message = main_commit.message
+
+            if len(commits) > 1:
+                # most recent commit
+                main_commit = sorted(commits, key=lambda c: c.authored_datetime)[-1]
+
+            commit_message += "\n\nautomatic commit by git-black, original commits:\n"
+            commit_message += "\n".join(["  {}".format(c.hexsha) for c in commits])
+
+            date_ts = main_commit.authored_date
+            date_tz = altz_to_utctz_str(main_commit.author_tz_offset)
+            self.repo.index.write()
+            self.repo.index.commit(
+                commit_message,
+                author=main_commit.author,
+                author_date="{} {}".format(date_ts, date_tz),
             )
-            self._blame_starts[filename] = []
-            self._blame_commits[filename] = []
-            for line, commit in _blame:
-                self._blame_starts[filename].append(line)
-                self._blame_commits[filename].append(commit)
-
-        idx = bisect(self._blame_starts[filename], lineno) - 1
-        return self._blame_commits[filename][idx]
-
-    @staticmethod
-    def compute_origin(delta: Delta):
-        """
-        return a dict that maps tuples of source lines
-        to tuples of destination lines. Each key/value pair
-        in the dict represents a set of source lines (the key tuple)
-        that became the destination lines (the value tuple).
-
-        although weird, the numbers in the tuples are 0-indexed.
-
-        e.g.
-        {   # src: dst
-            (0,1): (0)     # the first 2 lines collapsed into the first line of the output
-            (2,): (1,2,3)  # the third line expanded into lines 2 3 and 4
-        }
-        """
-
-        # this is harder than I thought; I'll start with a super naive
-        # approach and improve it later (or never)
-
-        if delta.src_length == 0:
-            return {(): tuple(range(delta.dst_length))}
-        if delta.dst_length == 0:
-            return {tuple(range(delta.src_length)): ()}
-
-        result = {}
-
-        for i in range(min(delta.src_length, delta.dst_length) - 1):
-            result[(i,)] = (i,)
-
-        if delta.src_length >= delta.dst_length:
-            result[tuple(range(delta.dst_length - 1, delta.src_length))] = (
-                delta.dst_length - 1,
-            )
-        else:
-            result[(delta.src_length - 1,)] = tuple(
-                range(delta.src_length - 1, delta.dst_length)
-            )
-
-        return result
 
     def commit_filename(self, filename):
         with TemporaryDirectory(dir=".") as tmpdir:
@@ -197,32 +357,8 @@ class GitBlack:
                 return
 
             mf = patch_set.modified_files[0]
-            hunk_deltas = [Delta.from_hunk(hunk, "latin1") for hunk in mf]
-
-            # let's map each hunk to its source commits and break down the deltas
-            # in smaller chunks; this will make it possible to prepare and group
-            # commits with a much smaller granularity
-            deltas = []
-            for hd in hunk_deltas:
-                for src_linenos, dst_linenos in self.compute_origin(hd).items():
-                    ss = hd.src_start + min(src_linenos, default=0)
-                    sl = [hd.src_lines[lineno] for lineno in src_linenos]
-                    ds = hd.dst_start + min(dst_linenos, default=0)
-                    dl = [hd.dst_lines[lineno] for lineno in dst_linenos]
-                    delta = Delta(
-                        src_start=ss, src_lines=sl, dst_start=ds, dst_lines=dl
-                    )
-                    deltas.append(delta)
 
             working_file = WorkingFile(original_lines, deltas)
-
-            delta_commits = {}
-            for delta_idx, delta in enumerate(deltas):
-                for line in range(
-                    delta.src_start, delta.src_start + max(1, delta.src_length)
-                ):
-                    commit = self.blame(filename, line)
-                    delta_commits.setdefault(delta_idx, set()).add(commit.hexsha)
 
             grouped_deltas = {}
             for delta_idx, commits in delta_commits.items():
@@ -262,15 +398,16 @@ class GitBlack:
                 )
 
 
-def git_black(filename):
-    gb = GitBlack()
-    gb.commit_filename(filename)
+# def git_black(filename):
+#     gb.commit_filename(filename)
 
 
 @click.command()
-@click.argument("filename")
-def cli(filename):
-    git_black(filename)
+# @click.argument("filename")
+def cli():
+    gb = GitBlack()
+    gb.commit_changes()
+    # git_black(filename)
 
 
 if __name__ == "__main__":
